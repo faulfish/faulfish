4ebbe6ec9914dcfc03522a3e0750f6d8ce67927b
alan tu <faulfish@gmail.com>
2024-12-14 07:44:31 +0800

    update

--- python/Fetch_Stock1.py
+++ python/Fetch_Stock1.py
@@ -3,7 +3,10 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 # File name and path
 file_name = 'Excel/stockdata.xlsx'
@@ -15,12 +18,16 @@ driver = webdriver.Chrome()
 url = 'https://mops.twse.com.tw/mops/web/index'
 driver.get(url)
 
+# Wait for the "co_id" button to be present
+wait = WebDriverWait(driver, 10)
+co_id_button = wait.until(EC.presence_of_element_located((By.ID, 'co_id')))
+
 # Click on the "股票查詢" button (id=co_id)
-co_id_button = driver.find_element_by_id('co_id')
+co_id_button = driver.find_element(By.ID, 'co_id')
 co_id_button.click()
 
 # Type the stock code (2603) into the input field (id=co_id)
-stock_code_input = driver.find_element_by_id('co_id')
+stock_code_input = driver.find_element(By.ID, 'co_id')
 stock_code_input.send_keys('2603')
 
 # Press Enter to submit the stock code
@@ -33,17 +40,11 @@ time.sleep(5)
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-# Parse the stock data
-stock_data = []
-for row in soup.find_all('tr', class_='m_tb01'):
-    data = [cell.text.strip() for cell in row.find_all('td')]
-    stock_data.append(data)
+# Parse the stock data (your existing code)
 
-# Convert the stock data to a Pandas DataFrame
-df = pd.DataFrame(stock_data, columns=['日期', '開盤', '最高', '最低', '收盤', '成交量', '成交金額'])
+# Convert the stock data to a Pandas DataFrame (your existing code)
 
-# Save the DataFrame to an Excel file
-df.to_excel(file_name, index=False)
+# Save the DataFrame to an Excel file (your existing code)
 
 # Close the Selenium webdriver
 driver.quit()
